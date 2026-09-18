7219394dcde516584c0432c6d880365b5507e6d5
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-09-18 10:57:48 -0400

    Changed the value of the dropdown selections to equal true and false and removed duplicate code from the addBookBtn event listener.

--- script.js
+++ script.js
@@ -107,71 +107,10 @@ addBookBtn.addEventListener("click", (e) => {
     const title = titleInput.value;
     const author = authorInput.value;
     const pages = pagesInput.value;
-    const read = readInput.value;
+    const isRead = readInput.value === "true";
 
-    const newBook = addBookToLibrary(title, author, pages, read);
-
-    const card = document.createElement("div")
-    const cardTitle = document.createElement("h2");
-    const cardAuthor= document.createElement("h3");
-    const cardPages = document.createElement("p");
-    const cardStatus = document.createElement("p");
-    const removeBtn = document.createElement("button");
-    const statusBtn = document.createElement("button");
-
-    card.dataset.id = newBook.id;
-
-    card.classList.add("card")
-
-    cardTitle.textContent = `Title: ${title}`;
-    cardTitle.classList.add("title");
-
-    cardAuthor.textContent = `Author: ${author}`;
-    cardAuthor.classList.add("author");
-
-    cardPages.textContent = `Pages: ${pages}`;
-    cardPages.classList.add("pages");
-
-    cardStatus.textContent = `Status: ${read}`;
-    cardStatus.classList.add("read");
-
-    removeBtn.textContent = "Remove";
-    removeBtn.classList.add("delete");
-
-    statusBtn.textContent = "Change Status";
-    statusBtn.classList.add("change-status");
-
-    card.append(cardTitle, cardAuthor, cardPages, cardStatus, removeBtn, statusBtn)
-
-    container.appendChild(card)
-
-    statusBtn.addEventListener("click", () => {
-        const bookId = card.dataset.id;
-        const targetBook = myLibrary.find(book => book.id === bookId);
-
-        if (targetBook) {
-            if (targetBook.read === "read") {
-                targetBook.read = "not read yet";
-            } else {
-                targetBook.read = "read";
-            }
-            cardStatus.textContent = `Status: ${targetBook.read}`
-        }
-    })
-
-    removeBtn.addEventListener("click", () => {
-        const bookId = card.dataset.id;
-        const bookIndex = myLibrary.findIndex(book => book.id === bookId);
-
-        if (bookIndex !== -1) {
-            myLibrary.splice(bookIndex, 1);
-        }
-
-        card.remove();
-        console.log("Updated Library:", myLibrary);
-    });
-    bookForm.reset();
-
+    addBookToLibrary(title, author, pages, isRead);
+    renderFilteredLibrary(myLibrary);
 });
 
 allBooksBtn.addEventListener("click", () => {
